9be90c25342116c0200b0808e9653dbc4daa98e9
artchieas720-uni <kaktusplayer04@gmail.com>
2026-06-20 13:02:35 +0200

    Add files via upload

---
+++ src/main/java/pl/pjatk/jaz34251nbp/Jaz34251NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz34251nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz34251NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jaz34251NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pjatk/jaz34251nbp/Jaz34251NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz34251nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz34251NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
